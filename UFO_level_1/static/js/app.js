38642c71ff4b4b185b9612b56ad910075f2f155f
Navilongo <navilongo@gmail.com>
2021-05-15 09:46:19 -0700

    Displayed each column and row into html

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -3,3 +3,13 @@ var tableData = data;
 
 //Append UFO data to table
 var tbody = d3.select('tbody')
+
+
+// Append data into table rows
+tableData.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
